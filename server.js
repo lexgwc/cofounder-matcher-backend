9cbcd78244a29c5cb87478c34e3455bca497dc91
Addy Jaime <addyjaime@gmail.com>
2024-04-29 10:17:12 -0400

    server running

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ import dotenv from 'dotenv/config.js';
 
 // Import modules
 import express from 'express';
+import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+// TODO: Import routes
+
 // Import database connection
+import './config/db.js'
 
-
+// Middleware
 app.use(cors());
-
-
+app.use(morgan('combined'))
+app.use(express.json())
  
 
 const app = express();
 
+// TODO: app.use routes
 
 app.get("/", (req, res)=>{
   console.log("hello world")
@@ -24,6 +29,8 @@ app.get("/", (req, res)=>{
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, )
+app.listen(PORT, ()=>{
+  console.log("Server running on port ", PORT)
+})
 
 
