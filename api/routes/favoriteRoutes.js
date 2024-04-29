8c48f82b923e09a19c27e3a1f7d72718aedb8a15
User Name <erikilla.pinzon@email.com>
2024-04-29 16:34:29 -0400

    update of favorite and School routes

---
+++ api/routes/favoriteRoutes.js
@@ -0,0 +1,29 @@
+// Import express and router
+
+import express from "express";
+
+const router = express.Router()
+
+// Routes
+
+// Get all favorites
+
+router.get('/', )
+
+// Get one favorite by id
+
+router.get('/:id', )
+
+// Create favorite 
+
+router.post('/', )
+
+// Update favorite by id
+
+router.put('/:id', )
+
+// Delete favorite by id
+
+router.delete('/:id', )
+
+export default router
---
+++ api/routes/schoolRoutes.js
@@ -0,0 +1,29 @@
+// Import express and router
+
+import express from "express";
+
+const router = express.Router()
+
+// Routes
+
+// Get all schools
+
+router.get('/', )
+
+// Get one school by id
+
+router.get('/:id', )
+
+// Create school
+
+router.post('/', )
+
+// Update school by id
+
+router.put('/:id', )
+
+// Delete school by id
+
+router.delete('/:id', )
+
+export default router
