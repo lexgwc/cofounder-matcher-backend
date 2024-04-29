017043ca48ab3873465661d4b11672b66e9f251b
User Name <erikilla.pinzon@email.com>
2024-04-29 13:54:16 -0400

    update of school Control

--- api/controllers/schoolController.js
+++ api/controllers/schoolController.js
@@ -1,1 +1,138 @@
-import School from '../models/schoolModel.js'
+import School from '../models/schoolModel.js' 
+
+// Get all schools
+
+export const getAllSchools
+ = async (req, res) => {
+  try {
+    const schools
+     = await School.find({})
+
+    if (!schools
+    ) {
+      res.status(400).json({
+        error: `No schools
+         found`
+      })
+    }
+
+    res.json(schools
+    )
+
+  } catch (error) {
+    res.status(500).json({
+      error: `Internal server error: ${error}`
+    })
+  }
+}
+
+// Get one School by ID
+
+export const getSchoolById = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const school = await School.findById(id)
+
+    if (!school) {
+      res.status(400).json({
+        error: `School not found`
+      })
+    }
+
+    res.json(School)
+
+  } catch (error) {
+    res.status(500).json({
+      error: `Internal server error: ${error}`
+    })
+  }
+}
+
+// Create a School
+
+export const createSchool = async (req, res) => {
+  try {
+    const schoolData = req.body
+
+    if (!schoolData.length === 0) {
+      res.status(401).json({
+        error: `No School data found`
+      })
+    }
+
+    const schoolCreated = await School.create(schoolData)
+
+    if (!schoolCreated) {
+      res.status(400).json({
+        error: `School was not able to be created`
+      })
+    }
+
+    res.json({
+      message: `School ${schoolData.name} was created successfully`
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      error: `Internal server error: ${error}`
+    })
+  }
+}
+
+// Update a School by ID
+
+export const updateSchoolByID = async (req, res) => {
+  try {
+    const { id } = req.params
+    const schoolData = req.body
+
+    const schoolToUpdate = await School.findById(id)
+
+    if (!schoolToUpdate) {
+      res.status(400).json({
+        error: `Could not find a School with ID ${id}`
+      })
+    }
+
+    Object.entries(schoolData).map((key, value) => {
+      schoolToUpdate[key] = value
+    })
+    
+    await schoolToUpdate.save()
+
+    res.json({
+      message: `School ${schoolData.name} was updated successfully`
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      error: `Internal server error: ${error}`
+    })
+  }
+}
+
+// Delete School by ID
+
+export const deleteSchoolByID = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const schoolToDelete = await School.findByIdAndDelete(id)
+
+    if (!schoolToDelete) {
+      res.status(400).json({
+        error: `School not found and was not deleted`
+      })
+    }
+
+    res.json({
+      message: `School was deleted successfully`
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      error: `Internal server error: ${error}`
+    })
+  }
+}
