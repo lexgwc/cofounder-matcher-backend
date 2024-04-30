46befa59b2f12cbc4f356bbdd4e5af68f868f56c
Josh Logan <joshlogan103@gmail.com>
2024-04-30 11:22:24 -0400

    Add auth routes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,14 +1,20 @@
 import express from "express";
 
+import { signup, login, verifyLoggedIn } from '../controllers/authController.js'
+
 const router = express.Router()
 
 // Allow a user to sign up
 
-
+router.post('/signup', signup)
 
 // Allow a user to login
 
+router.post('/signup', login)
+
 // Verifiy that a user is logged in
+
+router.post('/signup', verifyLoggedIn)
 
 export default router
 
