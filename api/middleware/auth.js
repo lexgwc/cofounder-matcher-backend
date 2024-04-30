77055f9b6f4f31807cc051b543829bd9baf8f150
Addy Jaime <addyjaime@gmail.com>
2024-04-30 11:05:43 -0400

    Middleware complete

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,2 +1,25 @@
+import "dotenv/config.js"
+import jwt from "jsonwebtoken"
 
+
+export const verifyAuth = (req, res, next) => {
+
+  const { authorization } = req.headers;
+  const token = authorization && authorization.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      error: 'No token provided'
+    })
+  }
+  jwt.verify(token, process.env.SECRET, (err, user) => {
+    if (err) {
+      return res.status(401).json({
+        error: 'Invalid token'
+      })
+    }
+    req.user = user
+    next()
+  })
+}
 
